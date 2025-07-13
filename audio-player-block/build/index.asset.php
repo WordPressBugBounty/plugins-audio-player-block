5bc04e9151a7b9fe09203122e40aaddb53c45465
Sean Murphy <sean@iamseanmurphy.com>
2025-07-13 05:17:50 +0000

    1.3.6

--- audio-player-block/build/index.asset.php
+++ audio-player-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => 'c8338a83a8a8dfaa7030');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '96a771299d10c021eaf1');
