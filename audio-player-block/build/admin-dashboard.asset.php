14708e8e5b74a3900c535a6e9017232cc97fff8e
Sean Murphy <sean@iamseanmurphy.com>
2026-07-13 17:22:33 +0000

    1.6.2

--- audio-player-block/build/admin-dashboard.asset.php
+++ audio-player-block/build/admin-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-i18n'), 'version' => 'd05ad92e09fcc9dd0422');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-i18n'), 'version' => 'c6aab6f348deab84adc9');
